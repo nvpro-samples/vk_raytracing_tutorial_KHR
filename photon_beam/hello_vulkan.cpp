7bd926a28e6d20eaed711db90bf401d704858fc6
dlimdlim <dlim7272@gmail.com>
2022-08-30 18:24:00 +0900

    set parameters

--- photon_beam/hello_vulkan.cpp
+++ photon_beam/hello_vulkan.cpp
@@ -285,7 +285,10 @@ void HelloVulkan::loadScene(const std::string& filename)
   std::vector<GltfShadeMaterial> shadeMaterials;
   for(const auto& m : m_gltfScene.m_materials)
   {
-    shadeMaterials.emplace_back(GltfShadeMaterial{m.baseColorFactor, m.emissiveFactor, m.baseColorTexture, m.metallicFactor, m.roughnessFactor});
+    float metallicFactor = m.metallicFactor;
+    if(m.metallicFactor < 0.1)
+      metallicFactor = 0.7;
+    shadeMaterials.emplace_back(GltfShadeMaterial{m.baseColorFactor, m.emissiveFactor, m.baseColorTexture, metallicFactor, m.roughnessFactor});
   }
   m_materialBuffer = m_alloc.createBuffer(cmdBuf, shadeMaterials,
                                           VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT);
@@ -1020,7 +1023,7 @@ void HelloVulkan::beamtrace(const nvmath::vec4f& clearColor)
       &regions[1], 
       &regions[2], 
       &regions[3], 
-      1, 1, 1024
+      1, 1, 4096
   );
 
   m_debug.endLabel(cmdBuf);
